ccb7b459b14218894106db3e9d60b78f142df1f1
Claes Östlund <claes@stenbergit.se>
2020-08-25 12:27:37 +0200

    1.0.0

---
+++ PluginButoUnique_rows.php
@@ -0,0 +1,29 @@
+<?php
+class PluginButoUnique_rows{
+  function __construct() {
+    wfPlugin::includeonce('wf/yml');
+    wfPlugin::enable('theme/include');
+  }
+  public function page_form(){
+    $element = new PluginWfYml(__DIR__.'/element/form.yml');
+    wfDocument::renderElement($element->get());
+  }
+  public function page_parse(){
+    $rows = preg_split("/\r\n/", wfRequest::get('text'));
+    $unique = array();
+    foreach ($rows as $key => $row) {
+      if(!isset($unique[$row])){
+        $unique[$row] = array('count' => 1, 'value' => $row);
+      }else{
+        $unique[$row]['count'] = $unique[$row]['count']+1;
+      }
+    }
+    $str = '';
+    foreach ($unique as $key => $value) {
+      $str .= $value['count']."\t".$value['value']."\n";
+    }
+    $element = new PluginWfYml(__DIR__.'/element/parse.yml');
+    $element->setByTag(array('str' => $str));
+    wfDocument::renderElement($element->get());
+  }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Buto-Plugin-ButoUnique_rows
+
+Post lines in a form and get unique rows along with a counter param.
+
+## Settings
+
+Set up page.
+```
+plugin_modules:
+  unique_rows:
+    plugin: 'buto/unique_rows'
+```
+
+## Usage
+
+Go to page.
+```
+/unique_rows/form
+```
